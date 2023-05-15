9a2420eefe52d5f744f810b7d2aa03995753b23a
LightDay <canec-007@bk.ru>
2023-05-15 18:13:26 +0300

    Добавьте файлы проекта.

---
+++ PasswdGenerator/CheckWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PasswdGenerator
+{
+    /// <summary>
+    /// Логика взаимодействия для CheckWindow.xaml
+    /// </summary>
+    public partial class CheckWindow : Window
+    {
+        public string ViewModel { get; set; }
+
+        public CheckWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void ShowViewModel()
+        {
+            MessageBox.Show(ViewModel);
+        }
+
+        private void MinButton_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void CloseButton_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ToolBar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+    }
+}
---
+++ PasswdGenerator/MainWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PasswdGenerator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //private void generateButton_Click(object sender, RoutedEventArgs e)
+        // {
+        //int passwordLength = (int)passwordLengthSlider.Value;
+        //bool includeSpecialChars = (bool)specialCharsCheckBox.IsChecked;
+        //bool includeUpperCase = (bool)upperCaseCheckBox.IsChecked;
+        //bool includeNumbers = (bool)numbersCheckBox.IsChecked;
+
+        //string password = GeneratePassword(passwordLength, includeSpecialChars, includeUpperCase, includeNumbers);
+        //passwordTextBox.Text = password;
+        //}
+
+        private string GeneratePassword(int length, bool includeSpecialChars, bool includeUpperCase, bool includeNumbers)
+        {
+            const string lowerCaseChars = "abcdefghijklmnopqrstuvwxyz";
+            const string specialChars = "!@#$%^&*()_+";
+            const string numbers = "0123456789";
+
+            StringBuilder passwordBuilder = new StringBuilder();
+            string allowedChars = lowerCaseChars;
+
+            if (includeSpecialChars)
+            {
+                allowedChars += specialChars;
+            }
+
+            if (includeUpperCase)
+            {
+                allowedChars += lowerCaseChars.ToUpper();
+            }
+
+            if (includeNumbers)
+            {
+                allowedChars += numbers;
+            }
+
+            using (var rng = new RNGCryptoServiceProvider())
+            {
+                byte[] data = new byte[length];
+                rng.GetBytes(data);
+
+                for (int i = 0; i < length; i++)
+                {
+                    int index = data[i] % allowedChars.Length;
+                    passwordBuilder.Append(allowedChars[index]);
+                }
+            }
+
+            return passwordBuilder.ToString();
+        }
+
+        private void CloseButton_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MinButton_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void ToolBar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+
+        private void BtnCheck_Click(object sender, RoutedEventArgs e)
+        {
+            CheckWindow checkWindow = new();
+            checkWindow.Show();
+            this.Close();
+        }
+    }
+}
