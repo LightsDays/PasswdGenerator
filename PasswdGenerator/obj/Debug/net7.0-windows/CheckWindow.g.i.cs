84f9bb85103694718f66b471e6d01b9d1d710cb3
LightDay <canec-007@bk.ru>
2023-05-15 19:22:33 +0300

    add button style

--- PasswdGenerator/obj/Debug/net7.0-windows/CheckWindow.g.i.cs
+++ PasswdGenerator/obj/Debug/net7.0-windows/CheckWindow.g.i.cs
@@ -77,7 +77,7 @@ namespace PasswdGenerator {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/PasswdGenerator;V1.0.0.0;component/checkwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/PasswdGenerator;component/checkwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\CheckWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
